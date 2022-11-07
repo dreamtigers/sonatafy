b6176d2f430d91cfe95cf54a50d7d14313adf1e4
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2022-11-07 09:44:27 -0500

    Change audited changes column type to jsonb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_142610) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_144038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_142610) do
     t.string "user_type"
     t.string "username"
     t.string "action"
-    t.text "audited_changes"
+    t.jsonb "audited_changes"
     t.integer "version", default: 0
     t.string "comment"
     t.string "remote_address"
