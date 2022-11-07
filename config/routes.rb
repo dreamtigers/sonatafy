dca3c1cf330bb3cb24ab3f780bc3b2fd7a9fadcb
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2022-11-07 08:23:34 -0500

    Add Dealership scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :dealerships
   resources :cars
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
